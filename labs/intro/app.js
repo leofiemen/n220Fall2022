75aa32d9ad406fa926326e33acba3780a91c253d
leofiemen <112003931+leofiemen@users.noreply.github.com>
2022-09-03 18:53:02 -0400

    changed MySmiley to smiley

--- labs/intro/app.js
+++ labs/intro/app.js
@@ -1,1 +1,12 @@
-document.write("hello world");
+function setup() {
+    createCanvas(800,600);
+
+    circle(60,60, 100)
+}
+
+
+function setup() {
+    createCanvas(100, 50);
+    background(153);
+    line(0, 0, width, height);
+  }
